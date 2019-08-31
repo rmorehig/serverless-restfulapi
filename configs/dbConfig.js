5cdc825544efbd02f71ddd1ae01a2c2c88814626
Rafa Moreno <rmh1516@gmail.com>
2019-08-31 11:51:34 +0200

    serverless deploy

---
+++ configs/dbConfig.js
@@ -0,0 +1,10 @@
+  
+const mysql = require('mysql')
+const pool  = mysql.createPool({
+  host            : 'localhost',
+  user            : 'root',
+  password        : 'Root@12345',
+  database        : 'pokemon_app_db',
+})
+
+module.exports = pool
